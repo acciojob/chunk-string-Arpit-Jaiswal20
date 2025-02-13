6f2d9d23059a9ce917426914a758f8df4a4378ce
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-02-13 14:06:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function stringChop(str, size) {
   // your code here
+	let val = [];
+    let i = 0;
+    while(i<str.length)
+    {
+        let s = i + size;  
+        let temp = "";
+        
+        while (i < s && i < str.length) { 
+            temp += str[i];
+            i++;
+        }
+        
+        val.push(temp);
+    }
+
+    return val;
 }
 
 // Do not change the code below
